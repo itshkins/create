2b619ad4665929753c7ca5eb2e708a81b828c111
itshkins <itshkins@gmail.com>
2023-11-07 13:16:08 +0300

    Fixes create script by commander

--- src/create.ts
+++ src/create.ts
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
-import {fileURLToPath} from 'url'
-import {join, dirname} from 'node:path'
+import {fileURLToPath} from 'node:url'
+import {join} from 'node:path'
 import fs from 'fs-extra'
+import {program} from 'commander'
+
+const {template, destination} = program
+  .requiredOption('-t, --template <template>', 'template')
+  .requiredOption('-d, --destination <destination>', 'destination path')
+  .parse()
+  .opts()
+console.log({template, destination})
 
 // @ts-ignore
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const cwd = process.cwd()
+const __dirname__ = fileURLToPath(import.meta.url)
+const __cwd__ = process.cwd()
 
-const [, , projectName] = process.argv
-const projectPath = join(cwd, projectName)
-
-const templateName = process.argv[1] || process.exit(1)
-const templatePath = join(__dirname, `../templates`, templateName)
+const projectPath = join(__cwd__, destination)
+const templatePath = join(__dirname__, `../templates`, template)
 
 const run = async () => {
   await fs.copy(templatePath, projectPath, {
