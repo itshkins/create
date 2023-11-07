4e090afe9765052afd4bea08fdc716e8ed573399
itshkins <itshkins@gmail.com>
2023-11-07 14:36:36 +0300

    fix

--- src/create.ts
+++ src/create.ts
@@ -17,7 +17,7 @@ const __cwd__ = process.cwd()
 
 const templatePath = join(__dirname__, `../../templates`, template)
 const projectPath = join(__cwd__, destination)
-console.log({__cwd__, templatePath, projectPath})
+console.log({__dirname__, __cwd__, templatePath, projectPath})
 
 const run = async () => {
   await fs.copy(templatePath, projectPath, {
