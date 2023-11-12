5ae0fae6229b2373c5c57c4fd6a60be513de5642
itshkins <itshkins@gmail.com>
2023-11-12 10:51:14 +0300

    Adds nuxt3 template

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 ```bash
 npm create @itshkins@latest -t backend-ts -d my-backend-project
+npm create @itshkins@latest -t nuxt3 -d my-frontend-project
 ```
---
+++ templates/nuxt3/app.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+Html(lang="ru")
+  Head
+    Title My App
+    Link(rel="preconnect" href="https://fonts.googleapis.com")
+    Link(rel="preconnect" href="https://fonts.gstatic.com" crossorigin)
+    Link(href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;900&display=swap" rel="stylesheet")
+  Body
+    NuxtLayout
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss">
+.app {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+</style>
+
